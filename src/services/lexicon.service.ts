6ead36baa63448af17a3d8af0cf9a376e63fcb66
Jake Shilling <shilling.jake@gmail.com>
2023-03-24 10:16:30 -0400

    FIX node to T mapping

--- src/services/lexicon.service.ts
+++ src/services/lexicon.service.ts
@@ -2,6 +2,7 @@ import { NodeService } from './node.service';
 import { Node } from '@/models/node/node.entity';
 import { InferType, object, Schema, string } from 'yup';
 import { NodeRepository } from '@/repositories/node/node.repository';
+import { FindOptionsWhere } from 'typeorm';
 
 export enum LexiconNodeType {
   Lexicon = 'lexicon',
@@ -23,15 +24,55 @@ export class CRUDService<T> {
 
   private async ofNode(node: Node): Promise<T> {
     const props = node.propertyKeys || [];
-    const obj = props.reduce(
-      (acc, key) => ({
-        ...acc,
-        [key.property_key]: key.propertyValue.property_value,
-      }),
-      {},
-    );
+    const obj = props.reduce((acc, key) => {
+      let value;
+      try {
+        value = JSON.parse(key.propertyValue.property_value);
+      } catch (err) {
+        console.warn(err);
+      }
 
-    return this.schema.cast(obj);
+      if (value && 'value' in value) {
+        return {
+          ...acc,
+          [key.property_key]: value.value,
+        };
+      } else {
+        return acc;
+      }
+    }, {});
+
+    try {
+      return this.schema.cast({ id: node.id, ...obj });
+    } catch (err) {
+      const msg = err instanceof Error ? err.message : 'unknown error';
+      const json = JSON.stringify(obj);
+      const desc = JSON.stringify(this.schema.describe());
+      throw new Error(
+        `Failed to cast object "${json}" to schema ${desc}: ${msg}`,
+      );
+    }
+  }
+
+  private findOptsFromWhere(opts: FindOptionsWhere<Node>) {
+    return {
+      where: opts,
+      relations: {
+        propertyKeys: {
+          propertyValue: true,
+        },
+      },
+    };
+  }
+
+  private async findNodeBy(opts: FindOptionsWhere<Node>): Promise<Node[]> {
+    return await this.nodeRepo.repository.find(this.findOptsFromWhere(opts));
+  }
+
+  private async findOneNodeBy(
+    opts: FindOptionsWhere<Node>,
+  ): Promise<Node | null> {
+    return await this.nodeRepo.repository.findOne(this.findOptsFromWhere(opts));
   }
 
   async create(obj: Partial<T>): Promise<T> {
@@ -40,16 +81,23 @@ export class CRUDService<T> {
       obj,
     );
 
-    return this.ofNode(node);
+    const entity = await this.findOneNodeBy({ id: node.id });
+    if (entity) {
+      return this.ofNode(entity);
+    } else {
+      throw new Error(
+        `Failed to create node from object: "${JSON.stringify(obj)}"`,
+      );
+    }
   }
 
   async findBy(opts: Partial<T>): Promise<T[]> {
-    const nodes = await this.nodeRepo.repository.findBy(opts);
+    const nodes = await this.findNodeBy(opts);
     return Promise.all(nodes.map(this.ofNode));
   }
 
   async findOneBy(opts: Partial<T>): Promise<T | null> {
-    const node = await this.nodeRepo.repository.findOneBy(opts);
+    const node = await this.findOneNodeBy(opts);
     return node ? this.ofNode(node) : null;
   }
 }
