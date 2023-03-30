b8de76d5f5916e4613c5e1e468fb1de077435b1a
hiroshi <hiroshi.tan57@gmail.com>
2023-03-30 22:31:04 +0000

    Add useDocument hooks

---
+++ src/hooks/useDocument.ts
@@ -0,0 +1,91 @@
+import { useCallback } from 'react';
+import { useAppContext } from '@/hooks/useAppContext';
+
+export function useDocument() {
+  const {
+    states: {
+      global: { singletons },
+    },
+    actions: { alertFeedback },
+  } = useAppContext();
+
+  const listDocument = useCallback(async () => {
+    if (!singletons) {
+      alertFeedback('error', 'Internal Error!');
+      return [];
+    }
+
+    try {
+      return singletons.graphThirdLayerService.listDocument();
+    } catch (err) {
+      console.log(err);
+      alertFeedback('error', 'Internal Error!');
+      return [];
+    }
+  }, [singletons, alertFeedback]);
+
+  const getDocument = useCallback(
+    async (name: string) => {
+      if (!singletons) {
+        alertFeedback('error', 'Internal Error!');
+        return [];
+      }
+
+      if (name.trim() === '') {
+        alertFeedback('warning', 'Document name cannot be empty string!');
+        return null;
+      }
+
+      try {
+        return singletons.graphThirdLayerService.getDocument(name);
+      } catch (err) {
+        console.log(err);
+        alertFeedback('error', 'Internal Error!');
+        return [];
+      }
+    },
+    [singletons, alertFeedback],
+  );
+
+  const createDocument = useCallback(
+    async (name: string) => {
+      if (!singletons) {
+        alertFeedback('error', 'Internal Error!');
+        return null;
+      }
+
+      if (name.trim() === '') {
+        alertFeedback('warning', 'Document name cannot be empty string!');
+        return null;
+      }
+
+      try {
+        const document = await singletons.graphThirdLayerService.getDocument(
+          name.trim(),
+        );
+
+        if (document) {
+          alertFeedback('warning', 'Already exists a document with same name!');
+          return null;
+        }
+        const newDocument =
+          await singletons.graphThirdLayerService.createDocument(name);
+
+        alertFeedback('success', 'Created a new document!');
+
+        return newDocument;
+      } catch (err) {
+        console.log(err);
+        alertFeedback('error', 'Internal Error!');
+        return null;
+      }
+    },
+    [singletons, alertFeedback],
+  );
+
+  return {
+    createDocument,
+    listDocument,
+    getDocument,
+  };
+}
