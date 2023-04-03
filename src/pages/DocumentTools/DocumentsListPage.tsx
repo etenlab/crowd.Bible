fc5a08e404c6b3b2971b2c43235585f3553a630d
hiroshi <hiroshi.tan57@gmail.com>
2023-04-03 16:19:32 +0000

    Update documentlists page with graph apis

--- src/pages/DocumentTools/DocumentsListPage.tsx
+++ src/pages/DocumentTools/DocumentsListPage.tsx
@@ -1,12 +1,21 @@
 import { IonContent } from '@ionic/react';
+
+import { Button } from '@eten-lab/ui-kit';
+
 import { LangugeSelectionBox } from '@/components/LanguageSelectionBox';
 import { DocumentList } from '@/components/DocumentList';
+import { Link } from '@/components/Link';
 
 export function DocumentsListPage() {
   return (
     <IonContent>
       <LangugeSelectionBox />
       <DocumentList />
+      <Link to="/add-document">
+        <Button variant="contained" sx={{ margin: '20px' }} fullWidth>
+          Add Document
+        </Button>
+      </Link>
     </IonContent>
   );
 }
