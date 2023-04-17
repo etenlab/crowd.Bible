6988b56be2e81a320fb2619502f41cc9e3c87cc7
hiroshi <hiroshi.tan57@gmail.com>
2023-04-17 23:31:41 +0000

    Refactor DocumentsListPage

--- src/pages/DocumentTools/DocumentsListPage.tsx
+++ src/pages/DocumentTools/DocumentsListPage.tsx
@@ -1,23 +1,120 @@
+import { useEffect, useState, useMemo } from 'react';
+import { useHistory } from 'react-router-dom';
+
 import { IonContent } from '@ionic/react';
 
-import { Button } from '@eten-lab/ui-kit';
+import { CrowdBibleUI, PlusButton, BiFile } from '@eten-lab/ui-kit';
 
-import { LangugeSelectionBox } from '@/components/LanguageSelectionBox';
-import { DocumentList } from '@/components/DocumentList';
+import { LanguageDto } from '@/dtos/language.dto';
+import { DocumentDto } from '@/src/dtos/document.dto';
+
+import { useAppContext } from '@/hooks/useAppContext';
+import { useLanguage } from '@/hooks/useLanguage';
+import { useDocument } from '@/hooks/useDocument';
+
 import { Link } from '@/components/Link';
 
+const { ButtonList, HeadBox } = CrowdBibleUI;
+
+type ButtonListItemType = CrowdBibleUI.ButtonListItemType;
+
 export function DocumentsListPage() {
+  const history = useHistory();
+
+  const { listDocument } = useDocument();
+  const { getLanguages } = useLanguage();
+
+  const [languageList, setLanguageList] = useState<LanguageDto[]>([]);
+  const [documents, setDocuments] = useState<DocumentDto[]>([]);
+  const [searchStr, setSearchStr] = useState<string>('');
+
+  const {
+    states: {
+      global: { user, singletons },
+      documentTools: { sourceLanguage, targetLanguage },
+    },
+    actions: { setSourceLanguage, setTargetLanguage },
+  } = useAppContext();
+
+  // Fetch language Lists from db
+  useEffect(() => {
+    if (singletons) {
+      getLanguages().then(setLanguageList);
+    }
+  }, [singletons, getLanguages]);
+
+  // Fetch Document Lists from db
+  useEffect(() => {
+    if (singletons) {
+      listDocument().then(setDocuments);
+    }
+  }, [listDocument, singletons]);
+
+  const handleSetSourceLanguage = (value: LanguageDto | null) => {
+    setSourceLanguage(value);
+  };
+
+  const handleSetTargetLanguage = (value: LanguageDto | null) => {
+    setTargetLanguage(value);
+  };
+
+  const handleChangeSearchStr = (str: string) => {
+    setSearchStr(str);
+  };
+
+  const handleClickDocument = (documentId: string) => {
+    if (user?.role === 'translator') {
+      history.push(`/translation/${documentId}`);
+    } else if (user?.role === 'reader') {
+      history.push(`/feedback/${documentId}`);
+    }
+  };
+
+  const items: ButtonListItemType[] = useMemo(() => {
+    return documents.map(({ id, name }) => ({
+      value: id,
+      label: name,
+    }));
+  }, [documents]);
+
   return (
     <IonContent>
-      <LangugeSelectionBox />
-      <DocumentList />
-      <Link to="/add-document">
-        <div style={{ width: '100%', padding: '20px' }}>
-          <Button variant="contained" fullWidth>
-            Add Document
-          </Button>
-        </div>
-      </Link>
+      <HeadBox
+        title="Documents"
+        languageSelector={{
+          languageList,
+          source: sourceLanguage,
+          target: targetLanguage,
+          onChangeSource: handleSetSourceLanguage,
+          onChangeTarget: handleSetTargetLanguage,
+        }}
+      />
+      <ButtonList
+        label="List of Docs"
+        search={{
+          value: searchStr,
+          onChange: handleChangeSearchStr,
+          placeHolder: 'Input Search Word...',
+        }}
+        withUnderline={true}
+        items={items}
+        startIcon={
+          <BiFile
+            style={{
+              borderRadius: '7px',
+              padding: '7px',
+              fontSize: '32px',
+              background: '#E3EAF3',
+            }}
+          />
+        }
+        onClick={handleClickDocument}
+        toolBtnGroup={
+          <Link to="/add-document">
+            <PlusButton variant="primary" onClick={() => {}} />
+          </Link>
+        }
+      />
     </IonContent>
   );
 }
