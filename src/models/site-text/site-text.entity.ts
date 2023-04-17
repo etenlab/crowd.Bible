48334526ae9a3b8029d6077b1bd5977e09029dd9
hiroshi <hiroshi.tan57@gmail.com>
2023-04-17 23:23:34 +0000

    Add site-text, and translation entities

---
+++ src/models/site-text/site-text.entity.ts
@@ -0,0 +1,34 @@
+import { Column, Entity, PrimaryColumn, BeforeInsert, Index } from 'typeorm';
+import { nanoid } from 'nanoid';
+
+import { Syncable } from '../Syncable';
+
+import { TableNameConst } from '@/constants/table-name.constant';
+
+@Entity({ name: TableNameConst.SITE_TEXT })
+@Index(['app_id', 'site_text_ref', 'definition_ref'], { unique: true })
+export class SiteText extends Syncable {
+  @PrimaryColumn('uuid', { type: 'varchar', length: 21, unique: true })
+  id!: string;
+
+  @BeforeInsert()
+  setId() {
+    this.id = nanoid();
+  }
+
+  // TODO: looks redundant, check and delete
+  @Column('text', { nullable: true })
+  readonly site_text_id!: string | null;
+
+  @Column('varchar')
+  app_id!: string;
+
+  @Column({ type: 'varchar' })
+  word_ref!: string;
+
+  @Column({ type: 'varchar' })
+  definition_ref!: string;
+
+  @Column({ type: 'varchar' })
+  original_language_id!: string;
+}
