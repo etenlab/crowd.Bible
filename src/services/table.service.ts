120f455d4948b0e3f1714a37172ecfefca0dab55
Ihor_S <ihor.shcherbyna@clockwise.software>
2023-04-18 11:42:33 +0200

    fix import

--- src/services/table.service.ts
+++ src/services/table.service.ts
@@ -6,7 +6,7 @@ import {
   RelationshipTypeConst,
   PropertyKeyConst,
 } from '@/constants/graph.constant';
-import { type Node } from '@/models/node/node.entity';
+import { type Node } from '@eten-lab/models';
 import { VotingService } from './voting.service';
 import { ElectionTypeConst } from '../constants/voting.constant';
 
