7eb9d9bd18193b202e7f1a0a101db765f6d6e130
hiroshi <hiroshi.tan57@gmail.com>
2023-04-20 22:30:00 +0000

    Integrate site-text service to the SiteTextTranslationEditorPage

--- src/pages/AppDev/SiteTextEditorPage.tsx
+++ src/pages/AppDev/SiteTextEditorPage.tsx
@@ -1,6 +1,7 @@
-import { ChangeEventHandler, useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { ChangeEventHandler, useState, useEffect, useCallback } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 import { IonContent } from '@ionic/react';
+import { useDebounce } from 'use-debounce';
 
 import {
   CrowdBibleUI,
@@ -10,14 +11,68 @@ import {
   MuiMaterial,
 } from '@eten-lab/ui-kit';
 
+import { useAppContext } from '@/hooks/useAppContext';
+import { useSiteText } from '@/hooks/useSiteText';
+import { MockApp, useLanguage } from '@/hooks/useLanguage';
+
+import { SelectableDefinitionCandidateList } from '@/components/SelectableDefinitionCandidateList';
+
+import { SiteTextDto } from '@/dtos/site-text.dto';
+
 const { HeadBox } = CrowdBibleUI;
 const { Stack } = MuiMaterial;
 
 export function SiteTextEditorPage() {
   const history = useHistory();
+  const { siteTextId, appId } = useParams<{
+    siteTextId?: Nanoid;
+    appId: Nanoid;
+  }>();
+  const {
+    states: {
+      global: { singletons },
+    },
+    actions: { alertFeedback },
+  } = useAppContext();
+  const { createSiteText, editSiteTextWordAndDescription, getSiteTextById } =
+    useSiteText();
+  const { getMockAppById } = useLanguage();
+
+  const [currentSiteText, setCurrentSiteText] = useState<SiteTextDto | null>(
+    null,
+  );
+  const [app, setApp] = useState<MockApp | null>(null);
 
   const [word, setWord] = useState<string>('');
+  const [bouncedWord] = useDebounce(word, 1000);
+
   const [description, setDescription] = useState<string>('');
+
+  const isEditor = siteTextId ? true : false;
+  // this is temporary solution because we don't have app service.
+  const siteTextLanguageId = isEditor
+    ? currentSiteText?.languageId
+    : app?.languageId;
+
+  // if editor mode, then fetch siteText
+  useEffect(() => {
+    if (singletons && siteTextId) {
+      getSiteTextById(siteTextId).then((data) => {
+        if (data) {
+          setWord(data.siteText);
+          setDescription(data.definition);
+        }
+        setCurrentSiteText(data);
+      });
+    }
+  }, [singletons, siteTextId, getSiteTextById]);
+
+  // Fetch site Lists from db
+  useEffect(() => {
+    if (singletons && appId) {
+      getMockAppById(appId).then(setApp);
+    }
+  }, [getMockAppById, singletons, appId]);
 
   const handleChangeWord: ChangeEventHandler<HTMLInputElement> = (event) => {
     setWord(event.target.value);
@@ -29,17 +84,64 @@ export function SiteTextEditorPage() {
     setDescription(event.target.value);
   };
 
-  const handleClickSave = () => {
-    history.goBack();
+  const handleChangeDescriptionForCandidateList = useCallback(
+    (description: string) => {
+      setDescription(description);
+    },
+    [],
+  );
+
+  const handleClickSave = async () => {
+    if (isEditor) {
+      const result = await editSiteTextWordAndDescription(
+        siteTextId!,
+        word,
+        description,
+      );
+
+      if (result) {
+        history.goBack();
+      }
+    } else {
+      if (appId && siteTextLanguageId) {
+        const result = await createSiteText(
+          appId,
+          siteTextLanguageId,
+          bouncedWord,
+          description,
+        );
+
+        if (result) {
+          history.goBack();
+        }
+      } else {
+        alertFeedback('error', 'Not exists appId');
+      }
+    }
   };
 
   const handleClickCancel = () => {
     history.goBack();
   };
 
+  const headCom = isEditor ? (
+    <HeadBox title="Edit Site Text" />
+  ) : (
+    <HeadBox title="Add New Site Txt" appTitle="App Name 1" />
+  );
+
+  const selectableCandidateListCom = siteTextLanguageId ? (
+    <SelectableDefinitionCandidateList
+      word={bouncedWord}
+      languageId={siteTextLanguageId}
+      description={description}
+      onChangeDescription={handleChangeDescriptionForCandidateList}
+    />
+  ) : null;
+
   return (
     <IonContent>
-      <HeadBox title="Add New Site Txt" appTitle="App Name 1" />
+      {headCom}
       <Stack gap="12px" sx={{ padding: '20px' }}>
         <Input
           label="Site Text"
@@ -63,6 +165,8 @@ export function SiteTextEditorPage() {
           Cancel
         </Button>
       </Stack>
+
+      {selectableCandidateListCom}
     </IonContent>
   );
 }
