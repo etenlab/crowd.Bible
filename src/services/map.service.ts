15be636a449f9b0dfff09442fb8325f61e569fef
hiroshi <hiroshi.tan57@gmail.com>
2023-04-25 17:10:10 +0000

    Split map.service from third-layer

---
+++ src/services/map.service.ts
@@ -0,0 +1,74 @@
+import { GraphFirstLayerService } from './graph-first-layer.service';
+import { GraphSecondLayerService } from './graph-second-layer.service';
+
+import { NodeTypeConst } from '@/constants/graph.constant';
+
+import { NodeRepository } from '@/repositories/node/node.repository';
+
+import { MapDto } from '@/dtos/map.dto';
+
+import { MapMapper } from '@/mappers/map.mapper';
+
+export class MapService {
+  constructor(
+    private readonly graphFirstLayerService: GraphFirstLayerService,
+    private readonly graphSecondLayerService: GraphSecondLayerService,
+
+    private readonly nodeRepo: NodeRepository,
+  ) {}
+
+  async saveMap(
+    langId: Nanoid,
+    mapInfo: {
+      name: string;
+      map: string;
+      ext: string;
+    },
+  ): Promise<Nanoid | null> {
+    const res =
+      await this.graphSecondLayerService.createRelatedFromNodeFromObject(
+        NodeTypeConst.MAP_LANG,
+        {},
+        NodeTypeConst.MAP,
+        mapInfo,
+        langId,
+      );
+    return res.node.id;
+  }
+
+  async getMap(mapId: Nanoid): Promise<MapDto | null> {
+    const langNode = await this.nodeRepo.repository.findOne({
+      relations: ['propertyKeys', 'propertyKeys.propertyValue'],
+      where: {
+        id: mapId,
+        node_type: NodeTypeConst.MAP,
+      },
+    });
+
+    if (!langNode) {
+      return null;
+    }
+
+    return MapMapper.entityToDto(langNode);
+  }
+
+  async getMaps(langId?: Nanoid) {
+    const mapNodes = await this.nodeRepo.repository.find({
+      relations: [
+        'propertyKeys',
+        'propertyKeys.propertyValue',
+        'toNodeRelationships',
+      ],
+      where: {
+        node_type: NodeTypeConst.MAP,
+        toNodeRelationships: {
+          relationship_type: NodeTypeConst.MAP_LANG,
+          to_node_id: langId,
+        },
+      },
+    });
+
+    const dtos = mapNodes.map((node) => MapMapper.entityToDto(node));
+    return dtos;
+  }
+}
