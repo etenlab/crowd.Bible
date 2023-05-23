6829f789d63e8b648865782f043f5275b98ced2a
hiroshi <hiroshi.tan57@gmail.com>
2023-05-23 03:06:52 +0000

    Refacotr routing

--- src/routes/DocumentToolsRoutes.tsx
+++ src/routes/DocumentToolsRoutes.tsx
@@ -1,10 +1,14 @@
 import { Route, Switch } from 'react-router-dom';
 
 import { DocumentsListPage } from '@/pages/DocumentTools/DocumentsListPage';
-import { NewDocumentAddPage } from '../pages/DocumentTools/NewDocumentAddPage';
+import { DocumentViewerPage } from '@/pages/DocumentTools/documentViewerPage';
+import { NewDocumentAddPage } from '@/pages/DocumentTools/NewDocumentAddPage';
+
+import { TranslationDocumentListPage } from '@/pages/DocumentTools/TranslationDocumentListPage';
 import { TranslationPage } from '@/pages/DocumentTools/TranslationPage';
-import { TranslationCandidatesPage } from '@/pages/DocumentTools/TranslationCandidatesPage';
+// import { TranslationCandidatesPage } from '@/pages/DocumentTools/TranslationCandidatesPage';
 import { TranslationEditPage } from '@/pages/DocumentTools/TranslationEditPage';
+
 import { FeedbackPage } from '@/pages/DocumentTools/FeedbackPage';
 import { TextPartFeedbackPage } from '@/pages/DocumentTools/TextPartFeedbackPage';
 import { ChapterFeedbackPage } from '@/pages/DocumentTools/ChapterFeedbackPage';
@@ -18,29 +22,39 @@ import { CommentaryPage } from '@/pages/DocumentTools/CommentaryPage';
 import { VersificationPage } from '@/pages/DocumentTools/VersificationPage';
 import { AlignmentPage } from '@/pages/DocumentTools/AlignmentPage';
 
+import { RouteConst } from '@/constants/route.constant';
+
 export function DocumentToolsRoutes() {
   return (
     <Switch>
-      <Route exact path="/documents-list">
+      <Route exact path={RouteConst.DOCUMENTS_LIST}>
         <DocumentsListPage />
       </Route>
-
-      <Route exact path="/add-document">
+      <Route exact path={`${RouteConst.DOCUMENTS_LIST}/:documentId`}>
+        <DocumentViewerPage />
+      </Route>
+      <Route exact path={`${RouteConst.ADD_DOCUMENT}`}>
         <NewDocumentAddPage />
       </Route>
 
-      <Route exact path="/translation/:documentId">
+      <Route exact path={`${RouteConst.TRANSLATION_DOCUMENTS_LIST}`}>
+        <TranslationDocumentListPage />
+      </Route>
+      <Route exact path={`${RouteConst.TRANSLATION}/:documentId`}>
         <TranslationPage />
       </Route>
-      <Route exact path="/translation-edit/:documentId/:wordSequenceId">
+      <Route
+        exact
+        path={`${RouteConst.TRANSLATION_EDIT}/:documentId/:wordSequenceId`}
+      >
         <TranslationEditPage />
       </Route>
-      <Route exact path="/translation-edit/:documentId">
+      <Route exact path={`${RouteConst.TRANSLATION_EDIT}/:documentId`}>
         <TranslationEditPage />
       </Route>
-      <Route exact path="/translation-candidates">
+      {/* <Route exact path={RouteConst.TRANSLATION_EDIT}>
         <TranslationCandidatesPage />
-      </Route>
+      </Route> */}
 
       <Route exact path="/feedback/:documentId">
         <FeedbackPage />
